ef6f1b5a831018aaad81f185cd0c6e3f1daf4cab
[Pratheek Gowda] <pratheekgowdadj@gmail.com>
2022-09-07 10:55:53 +0530

    first problem

--- FIBONACCI.java
+++ FIBONACCI.java
@@ -1,14 +1,18 @@
 public class FIBONACCI {
     public static void main(String[] args) {
-        System.out.println(fibo(0));
+        for (int i = 0; i < 10 ; i++) {
+
+
+            System.out.println(fibo(i));
+        }
     }
 
     static int fibo(int n){
 
-        if(n <= 0) return -1;
-        if(n == 1)
-            return 1;
 
-        return n * fibo(n-1);
+        if(n <= 1)
+            return n;
+
+        return fibo(n - 1) + fibo(n - 2);
     }
 }
